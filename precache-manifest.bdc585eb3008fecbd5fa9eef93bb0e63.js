69990622bf716b6b990b1aa746ccba918fd960df
hmbeale <holden.beale@gmail.com>
2019-03-09 10:27:13 -0500

    Updates

---
+++ precache-manifest.bdc585eb3008fecbd5fa9eef93bb0e63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4dacabf680aae804f7c",
+    "url": "/dune-ipsum/static/js/runtime~main.d4dacabf.js"
+  },
+  {
+    "revision": "d00317dec94f141195bc",
+    "url": "/dune-ipsum/static/js/main.d00317de.chunk.js"
+  },
+  {
+    "revision": "b89a01d589265437edc6",
+    "url": "/dune-ipsum/static/js/2.b89a01d5.chunk.js"
+  },
+  {
+    "revision": "d00317dec94f141195bc",
+    "url": "/dune-ipsum/static/css/main.4ce8c9bb.chunk.css"
+  },
+  {
+    "revision": "7ecee4525657f4514ab5a62379774d04",
+    "url": "/dune-ipsum/index.html"
+  }
+];
